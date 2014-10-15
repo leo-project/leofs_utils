f5b332ba09ca30e5b957f71453c93e393fe99f69
mocchira <nekotaroh@gmail.com>
2014-10-15 10:15:48 +0900

    Refactor

--- tools/b2l/leofs_b2l.erl
+++ tools/b2l/leofs_b2l.erl
@@ -29,47 +29,15 @@
 -define(DEF_LEVELDB_DIR_PATH, "./").
 
 main(Args) ->
-    %% Get a string repr of build time
-    Built = build_time(),
-
-    %% Get a string repr of first matching VCS changeset
-    VcsInfo = vcs_info([{hg, ".hg", "hg identify -i", "hg status"},
-                        {git, ".git", "git describe --always --tags",
-                         "git status -s"}]),
-
-    %% Check for force=1 flag to force a rebuild
-    case lists:member("force=1", Args) of
-        true ->
-            rm("ebin/*.beam");
-        false ->
-            rm("ebin/leofs_b2l.beam")
-    end,
-
-    %% Compile all src/*.erl to ebin
-    OtpInfo = string:strip(erlang:system_info(otp_release), both, $\n),
-    case make:files(filelib:wildcard("src/*.erl"),
-                    [{outdir, "ebin"}, {i, "include"},
-                     {d, 'BUILD_TIME', Built},
-                     {d, 'VCS_INFO', VcsInfo},
-                     {d, 'OTP_INFO', OtpInfo}]) of
-        up_to_date ->
-            ok;
-        error ->
-            io:format("Failed to compile leofs_b2l files!\n"),
-            halt(1)
-    end,
-
+    
     %% Make sure file:consult can parse the .app file
-    case file:consult("ebin/leofs_b2l.app") of
+    case file:consult("leofs_b2l.app") of
         {ok, _} ->
             ok;
         {error, Reason} ->
-            io:format("Invalid syntax in ebin/leofs_b2l.app: ~p\n", [Reason]),
+            io:format("Invalid syntax in leofs_b2l.app: ~p\n", [Reason]),
             halt(1)
     end,
-
-    %% Add ebin/ to our path
-    true = code:add_path("ebin"),
 
     %% Add third party ebin to our path
     EbinDirs = filelib:wildcard("./deps/*/ebin"),
@@ -181,42 +149,4 @@ option_spec_list() ->
      {leveldb_dir, $l, "leveldb_dir", string, "Specify a leveldb directory"},
      {version,     $v, "version", undefined,  "Show version information"}
     ].
-
-%% @doc Retrieve build time
-%% @private
-build_time() ->
-    {{Y, M, D}, {H, Min, S}} = calendar:now_to_universal_time(now()),
-    lists:flatten(io_lib:format("~4..0w~2..0w~2..0w_~2..0w~2..0w~2..0w",
-                                [Y, M, D, H, Min, S])).
-
-%% @doc
-%% @private
-vcs_info([]) ->
-    "No VCS info available.";
-vcs_info([{Id, Dir, VsnCmd, StatusCmd} | Rest]) ->
-    case filelib:is_dir(Dir) of
-        true ->
-            Vsn = string:strip(os:cmd(VsnCmd), both, $\n),
-            Status = case string:strip(os:cmd(StatusCmd), both, $\n) of
-                         [] ->
-                             "";
-                         _ ->
-                             "-dirty"
-                     end,
-            lists:concat([Id, " ", Vsn, Status]);
-        false ->
-            vcs_info(Rest)
-    end.
-
-
-%% @doc Remove a file
-%% @private
-rm(Path) ->
-    NativePath = filename:nativename(Path),
-    Cmd = case os:type() of
-              {unix,_} -> "rm -f ";
-              {win32,_} -> "del /q "
-          end,
-    [] = os:cmd(Cmd ++ NativePath),
-    ok.
 
